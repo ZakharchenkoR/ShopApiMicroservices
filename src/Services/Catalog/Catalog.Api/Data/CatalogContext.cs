74a5ac2414d8bedc40e59ce6c7c04330ba4fb0bb
Roman Zakharchenko <superzaec22@gmail.com>
2021-05-06 18:29:47 +0300

    create CatalogContext

---
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            var databaseName = configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+            var collectionName = configuration.GetValue<string>("DatabaseSettings:CollectionName");
+
+            var client = new MongoClient(connectionString);
+            var dataBase = client.GetDatabase(databaseName);
+            Prodacts = dataBase.GetCollection<Product>(collectionName);
+
+            CatalogContextSeed.SeedData(Prodacts);
+        }
+
+        public IMongoCollection<Product> Prodacts { get; }
+    }
+}
