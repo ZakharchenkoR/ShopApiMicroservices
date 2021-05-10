dfde635a21b06e579307368ac5e7837cd2fa7048
Roman Zakharchenko <superzaec22@gmail.com>
2021-05-10 11:28:56 +0300

    create ShoppingCart and ShoppingCartItem entities

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            Items = new List<ShoppingCartItem>();
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+            Items = new List<ShoppingCartItem>();
+        }
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+	            {
+                    totalPrice += item.Quantity * item.Price;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
