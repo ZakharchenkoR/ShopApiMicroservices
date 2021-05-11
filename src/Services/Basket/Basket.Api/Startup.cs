0dc18c21dddee5de20e56253fa7c5c2f7340abae
Roman Zakharchenko <superzaec22@gmail.com>
2021-05-11 13:52:13 +0300

    create BasketController

--- src/Services/Basket/Basket.Api/Startup.cs
+++ src/Services/Basket/Basket.Api/Startup.cs
@@ -1,16 +1,11 @@
+using Basket.Api.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.Api
 {
@@ -29,6 +24,8 @@ namespace Basket.Api
             {
                 option.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
+
+            services.AddScoped<IBasketRepository, BasketRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
